a8acd460adfdf4283e6dfba388954f062169772f
deyi <641389469@qq.com>
2026-01-15 01:31:05 +0800

    ci(website): use edge functions

--- apps/website/declaration.d.ts
+++ apps/website/declaration.d.ts
@@ -1,3 +1,9 @@
 declare module 'http://*';
 
 declare module 'https://*';
+
+declare module 'virtual:netlify-server-entry' {
+  import type { ServerEntryModule } from 'react-router';
+  const entry: ServerEntryModule;
+  export default entry;
+}
